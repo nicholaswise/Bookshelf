9c369ca2f8f50ec9e58e1e7e935df442f79a6de7
Nick Wise <nickcwise@gmail.com>
2017-07-24 15:01:31 -0400

    more stuff

--- Bookshelf/Models/Category.cs
+++ Bookshelf/Models/Category.cs
@@ -10,6 +10,8 @@ namespace Bookshelf.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [Display(Name="Category")]
         public string Name { get; set; }
         public virtual ICollection<Book> Books { get; set; }//<--Navigation Property
 
